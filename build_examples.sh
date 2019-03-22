acc142c53d9bbcc1003718a89b2ed1c6a90780a1
U-WESTERNEXP\eodell <eodell@GP4XST2.westernexp.com>
2019-03-22 10:46:30 -0500

    Example script added

--- build_examples.sh
+++ build_examples.sh
@@ -3,4 +3,4 @@
 # NOTE: This script assumes that Experior is installed globally. You'll have to
 # make adjustments otherwise.
 
-
+experior -i test_output.txt -r regression_data.json -R -j jstests.js -l -o ansi test_report.txt test_report.csv test_report.html test_report.json
